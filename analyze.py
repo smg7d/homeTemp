be5205146dc6ff4c0e7d5771f73cafaa3ddec1bc
smg7d <shane.m.gallagher@gmail.com>
2019-08-03 14:15:06 -0400

    finish analysis and chart plotting

--- analyze.py
+++ analyze.py
@@ -2,6 +2,8 @@ import os, sys
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
 
 
 dataFiles = ["tempData1.csv", "tempData2.csv", "tempData3.csv", "tempData4.csv", "tempData5.csv"]
@@ -9,10 +11,28 @@ dataFiles = ["tempData1.csv", "tempData2.csv", "tempData3.csv", "tempData4.csv",
 fullList = []
 
 for thisfile in dataFiles:
-	tempDF = pd.read_csv(thisfile, header=None, names=['temp', 'date'], index_col=1)
+	#read in data frame
+	tempDF = pd.read_csv(thisfile, header=None, names=['temp', 'date'])
+
+	#format the date column
+	tempDF['date'] = pd.to_datetime(tempDF['date'])
+
+	#set the date as the index
+	tempDF = tempDF.set_index('date')
+
+	#append it
 	fullList.append(tempDF)
 
+#put all of them together
 fullData = pd.concat(fullList)
 
-ax = sns.lineplot(data=fullData)
-plt.show()
+#create the axis object and format the plot
+palette = sns.color_palette("mako_r", 1)
+ax = sns.lineplot(data=fullData, palette=palette)
+ax.xaxis.set_major_locator(mdates.AutoDateLocator()) #set start of x axis
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) #format the x axis labels
+ax.set(xlabel='Date', ylabel='Temperature (Fahrenheit)', title='House Temperature Data') #add axis labels
+ax.legend_.remove() #take out the legend
+
+
+plt.savefig('houseTempData')
